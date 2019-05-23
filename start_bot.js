dc439bb6ec3f3fda5afcd8f03cf6f445c263a031
Yash <yash@logiqids.com>
2019-05-23 17:31:23 +0530

    Setting the bot to the initial sample file

--- start_bot.js
+++ start_bot.js
@@ -3,16 +3,15 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     // console.log('I\'m Alive!');  
     // console.log(tab.url);
     chrome.tabs.executeScript(tab.ib, {
-            file: 'lib/run_bot.js'
+            file: 'lib/bot.js'
     });
 
-    // chrome.tabs.executeScript({
-    //   code: `
-    //       console.log(tab.url);
-    //       console.log('I am Alive!');
-    //       start_bot()
-    //       console.log('Its done!');
-    //   `
-    // }); 
+    chrome.tabs.executeScript({
+      code: `
+          console.log('I am Alive!');
+          start_bot()
+          console.log('Its done!');
+      `
+    }); 
 
 });
